d170537521dd1479608ac93c4ed7877410744acf
Boudjenane Rachid <rboudjenane@jehann.fr>
2023-11-21 16:24:23 +0100

    init project :rocket:

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+export default class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>App Component</div>
+    )
+  }
+}
+
+
